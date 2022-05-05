2d4d878e07ec3075a526927fc261527cbe18a609
SeongWooSeok90 <101170781+SeongWooSeok90@users.noreply.github.com>
2022-05-05 23:49:06 +0900

    Initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,28 @@
+public class Helloworld {
+    public static  void  main(String[]args){
+        add();
+        b();
+        c();
+        d();
+    }
+    public static  void  add(){
+        int a= 5;
+        System.out.println(a+a);
+    }
+
+    public static  void  b(){
+        int a= 5;
+        System.out.println(a-a);
+    }
+    public static  void  c(){
+        int a= 5;
+        System.out.println(a*a);
+    }
+
+    public static  void  d(){
+        int a= 5;
+        System.out.println(a%a);
+    }}
+
+
+
